7204406f845e4251cc8eda046d893e2493ce98b9
pooya <pooyaamirsadeghi.1384@gmail.com>
2025-09-25 22:47:08 +0330

    added html template

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+import {toggleNewTodoContainer, newTodoContainerBtn} from "./utils/utils.js";
+
+
+newTodoContainerBtn.addEventListener('click', () => {
+    toggleNewTodoContainer();
+});
+
+const descriptionBtn = document.querySelectorAll('.descriptionBtn');
+
+descriptionBtn.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        const isOpen = btn.getAttribute('aria-expended') === 'true';
+        const icon = btn.querySelector('svg');
+
+        if (isOpen) {
+            btn.parentElement.style.maxHeight = '24px';
+            btn.setAttribute('aria-expended', 'false');
+            icon.classList.remove('rotate-180');
+        }else {
+            btn.parentElement.style.maxHeight = btn.parentElement.scrollHeight + 'px';
+            btn.setAttribute('aria-expended', 'true');
+            icon.classList.add('rotate-180');
+        }
+    })
+})
---
+++ public/js/utils/utils.js
@@ -0,0 +1,33 @@
+const newTodoContainer = document.querySelector('#newTodoContainer');
+const newTodoForm = document.querySelector('#newTodoForm');
+const newTodoContainerBtn = document.querySelector('#newTodoContainerBtn');
+
+const toggleNewTodoContainer = () => {
+    const isOpen = newTodoContainerBtn.getAttribute('aria-expanded') === 'true';
+
+    const icon = newTodoContainerBtn.querySelector('#plusCircleIcon');
+    const height = newTodoContainer.scrollHeight;
+
+    if (isOpen) {
+        newTodoForm.setAttribute('aria-hidden', 'true');
+        newTodoContainerBtn.setAttribute('aria-expanded', 'false');
+        newTodoContainerBtn.classList.add('overflow-hidden');
+        // newTodoContainer.classList.replace(`max-h-[${height}px]`, 'max-h-[56px]');
+        newTodoContainer.style.maxHeight = '56px'
+        icon.classList.remove('rotate-45');
+    }else {
+        newTodoForm.setAttribute('aria-hidden', 'false');
+        newTodoContainerBtn.setAttribute('aria-expanded', 'true');
+        newTodoContainerBtn.classList.remove('overflow-hidden');
+        // newTodoContainer.classList.replace('max-h-[56px]', `max-h-[${height}px]`);
+        newTodoContainer.style.maxHeight = height + 'px'
+        icon.classList.add('rotate-45');
+    }
+
+}
+
+
+export {
+    toggleNewTodoContainer,
+    newTodoContainerBtn
+}
